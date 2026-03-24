b1d5467c0387c261e1306554fddc16b87d80332d
sebascorrales-dev <sebastian.corrales@bibisoft.co>
2026-03-24 12:53:33 -0500

    chore: update PDB and assembly info files for release and debug configurations

--- biometric-service/obj/Debug/net8.0-windows/BiometricService.AssemblyInfo.cs
+++ biometric-service/obj/Debug/net8.0-windows/BiometricService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiometricService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f769de960b4bf438f3e11c925fbefb7a75e27b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5123e087188f9681be3ba1ad67a57abf5233344")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiometricService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiometricService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
